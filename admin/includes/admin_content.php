e54b2722c94b9e94177be46e77d5140c8fbd1622
Mariya Lucas <mariya.podosinova@gmail.com>
2019-11-19 17:29:18 +0000

    Assigning Array Values to Object Properties

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -10,14 +10,21 @@
                         
                         <?php
                             
-                            $result_set = User::find_all_users();
-                            while($row = mysqli_fetch_array($result_set)){
-                                echo $row["username"] . "</br>";
-                            }
+                            //$result_set = User::find_all_users();
+                            // while($row = mysqli_fetch_array($result_set)){
+                            //     echo $row["username"] . "</br>";
+                            // }
 
                             $found_user = User::find_user_by_id(2);
-                            echo  $found_user["username"];
-                                                       
+                            $user = new User();
+
+                            $user->id = $found_user["id"];
+                            $user->username = $found_user["username"];
+                            $user->password = $found_user["password"];
+                            $user->first_name = $found_user["first_name"];
+                            $user->last_name = $found_user["last_name"];
+                            
+                            echo $user->id;
                         ?>
 
 
