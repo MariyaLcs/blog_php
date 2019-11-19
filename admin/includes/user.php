4653b4ad9f125a80e0e30655602bd168c46b3297
Mariya Lucas <mariya.podosinova@gmail.com>
2019-11-19 16:46:59 +0000

    Creating a Find user id method and Solution

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -8,6 +8,12 @@ public static function find_all_users(){
     return $result_set;
 }
 
+public static function find_user_by_id($user_id){
+    global $database;
+    $result_set = $database->query("SELECT * FROM users WHERE id=$user_id");
+    $found_user = mysqli_fetch_array($result_set);
+    return $found_user;
+}
 
 }
 
