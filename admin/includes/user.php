6226d1bcee13d811a64e68660a01debec07312b3
Mariya Lucas <mariya.podosinova@gmail.com>
2019-11-19 19:17:08 +0000

    Creating The Attribute Finder Method

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -43,6 +43,12 @@ public static function instantiation($the_record){
     return $the_object;
 }
 
+private function has_the_attribute($the_attribute){
+    $object_properties = get_object_vars($this);
+
+   return array_key_exists($the_attribute, $object_properties);
+}
+
 }
 
 
