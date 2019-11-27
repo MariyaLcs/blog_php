73483f0ebf2d16a2f65aa88bc9724c203eb60f10
Mariya Lucas <mariya.podosinova@gmail.com>
2019-11-27 16:26:02 +0000

    The Checking login Method

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -2,11 +2,21 @@
 
 class Session{
 
-    private $signied_in;
+    private $signed_in;
     public $user_id;
 
 function __construct(){
     session_start();
+}
+
+private function check_the_login(){
+    if(isset($_SESSION['user_id'])){
+        $this->user_id = $_SESSION['user_id'];
+        $this->singed_in = true;
+    } else {
+        unset($this->user_id);
+        $this->singed_in = false;
+    }
 }
 
 }
