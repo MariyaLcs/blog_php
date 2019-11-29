9142eace90282a9a7539e4a481d9ee6f3ec83f28
Mariya Lucas <mariya.podosinova@gmail.com>
2019-11-29 15:08:51 +0000

    Outputting Feedback for User

--- admin/login.php
+++ admin/login.php
@@ -1,10 +1,7 @@
 <?php require_once("includes/header.php"); ?>
-
 <?php
 
-if($session->is_signed_in()){
-redirect("index.php");
-}
+if($session->is_signed_in()){redirect("index.php");}
 
 if(isset($_POST['submit'])){
 
@@ -14,7 +11,6 @@ $password = trim($_POST['password']);
 //Method to check database user
 
 $user_found = User::verify_user($username, $password);
-
 
 if($user_found){
 
@@ -26,24 +22,34 @@ redirect("index.php");
 }
 
 } else {
+/*message() method accepts argument $msg, which by default if nothing is provided holds an empty string.
+If we do have provided the argument, then it will store it inside the session.
+If we don't, it will return the existing message property
+That's all about that method.
+
+check_message() method first checks if the message is set inside the session, and if it is, it assigns it 
+to the message property, then simply unsets it from the session. But if the message is not present in the session, 
+it simply resets the message property to an empty string. So you can't set the message to an empty 
+string with: $this->message; but you need to assign it a value of an empty string! $this->message = '';*/
+	$the_message = "";
     $username = "";
     $password = "";
 }
-
-
 ?>
 <div class="col-md-4 col-md-offset-3">
+
+<h4 class="bg-danger"><?php echo $the_message;?></h4>
 
 <form action="" method="post">
 	
 	<div class="form-group">
 		<label for="username">Username</label>
-		<input type="text" class="form-control" name="username" value="" >
+		<input type="text" class="form-control" name="username" value="<?php echo htmlentities($username); ?>" >
 	</div>
 
 	<div class="form-group">
 		<label for="password">Password</label>
-		<input type="password" class="form-control" name="password" value="">
+		<input type="password" class="form-control" name="password" value="<?php echo htmlentities($password); ?>">
 	</div>
 	<div class="form-group">
 			<input type="submit" name="submit" value="Submit" class="btn btn-primary">
