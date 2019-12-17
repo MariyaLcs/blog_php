e80430362fb2e242aec735062b4ad5055fac6079
Mariya Lucas <mariya.podosinova@gmail.com>
2019-12-17 13:54:13 +0000

    Inserting Last ID Method

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -49,6 +49,10 @@ class Database {
     public function the_insert_id(){
         return $this->connection->insert_id;
     }
+
+    public function the_insert_id(){
+        return mysqli_insert_id($this->connection);
+    }
 }
 
 $database = new Database();
