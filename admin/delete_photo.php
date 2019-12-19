1c77b02acc8275d4053cdc9549b01c1ae6d3275e
Mariya Lucas <mariya.podosinova@gmail.com>
2019-12-19 18:38:20 +0000

    Creating The Delete Method

--- admin/delete_photo.php
+++ admin/delete_photo.php
@@ -2,14 +2,15 @@
 <?php if(!$session->is_signed_in()){redirect("login.php");}?>
 <?php
 
-if(empty($_GET['photo_id'])){
+if(empty($_GET['id'])){
         redirect("photos.php");
 }
 
-$photo = Photo::find_by_id(['photo_id']);
+$photo = Photo::find_by_id(['id']);
 
 if($photo){
     $photo->delete_photo();
+    redirect("photos.php");
 }else{
     redirect("photos.php");
 }
