95174532c0d89de33e8378bf63bf93612680cb7b
Mariya Lucas <mariya.podosinova@gmail.com>
2019-12-20 16:09:14 +0000

    Installig The Text Editor

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -7,6 +7,10 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
+    <!-- WYSIWYG -->
+    <script src="https://tinymce.cachefly.net/4.1/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="js/scripts.js"></script>
+
 </body>
 
 </html>
