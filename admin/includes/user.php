0a35ecc33adb97ee3215496ce65f01cc64de10b6
Mariya Lucas <mariya.podosinova@gmail.com>
2019-12-20 17:06:59 +0000

    Working with User Image

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -3,29 +3,36 @@
 class User extends Db_object{
 
     protected static $db_table = "users";
-    protected static $db_table_fields = array('username', 'password', 'first_name', 'last_name');
+    protected static $db_table_fields = array('username', 'password', 'first_name', 'last_name', 'user_image');
     public $id;
     public $username;
     public $password;
     public $first_name;
     public $last_name;
+    public $user_image;
+    public $upload_directory = "images";
+    public $image_placeholder = "http://placehold.it/400x400&text=image";
+
+    public function image_path_and_placeholder(){
+        return empty($this->user_image) ? $this->image_placeholder : $this->$upload_directory . DS . $this->user_image;
+    }
 
 
     public static function verify_user($username, $password){
 
-    global $database;
+        global $database;
 
-    $username = $database->escape_string($username);
-    $password = $database->escape_string($password);
+        $username = $database->escape_string($username);
+        $password = $database->escape_string($password);
 
-    $sql = "SELECT * FROM ". self::$db_table . " WHERE ";
-    $sql .= "username = '{$username}' ";
-    $sql .= "AND password = '{$password}' ";
-    $sql .= "LIMIT 1";
+        $sql = "SELECT * FROM ". self::$db_table . " WHERE ";
+        $sql .= "username = '{$username}' ";
+        $sql .= "AND password = '{$password}' ";
+        $sql .= "LIMIT 1";
 
-    $the_result_array = self::find_by_query($sql);
+        $the_result_array = self::find_by_query($sql);
 
-    return !empty($the_result_array) ? array_shift($the_result_array) : false;
+        return !empty($the_result_array) ? array_shift($the_result_array) : false;
     }
 
     
--- admin/users.php
+++ admin/users.php
@@ -47,7 +47,7 @@ $users = User::find_all();
 
                                     <tr>
                                         <td><?php echo $user->id; ?></td>
-                                        <td><img class="admin-user-thumbna" src="<?php echo $user->user_image; ?>" alt=""></td>
+                                        <td><img class="admin-user-thumbnail user_image" src="<?php echo $user->image_path_and_placeholder(); ?>" alt=""></td>
                                         
                                         <td><?php echo $user->username; ?>
                                             <div class="action_links">
