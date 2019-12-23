7cf16126c4f8e637b24ab2d7ea05c350976c3fa7
Mariya Lucas <mariya.podosinova@gmail.com>
2019-12-23 20:38:24 +0000

    Creating the Individual Comment Page

--- admin/comment_photo.php
+++ admin/comment_photo.php
@@ -2,7 +2,13 @@
 <?php if(!$session->is_signed_in()){redirect("login.php");}?>
 
 <?php
-$photos = Photo::find_all();
+if(empty($_GET['id'])){
+    redirect ("photos.php");
+}
+
+$comments = Comment::find_the_comments($_GET['id']);
+
+
 
 ?>
 
@@ -27,36 +33,32 @@ $photos = Photo::find_all();
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Photos
-                            <small>Subheading</small>
+                            Users                            
                         </h1>
+                        <a href="add_user.php" class="btn btn-primary">Add User</a>
                         <div class="col-md-12">
                             <table class="table table-hover">
                                 <thead>
-                                    <tr>
-                                        <th>Photo</th>
+                                    <tr>                                        
                                         <th>Id</th>
-                                        <th>File Name</th>
-                                        <th>Title</th>
-                                        <th>Size</th>
+                                        <th>Author</th>
+                                        <th>Body</th>
                                     </tr>
                                 </thead>
                                 <tbody>
 
-                                    <?php foreach($photos as $photo) : ?>                              
+                                    <?php foreach($comments as $comment) : ?>                              
 
                                     <tr>
-                                        <td><img class="admin-photo-thumbna" src="<?php echo $photo->picture_path(); ?>" alt="">
-                                            <div class="action_link">
-                                                <a href="delete_photo.php?id=<?php echo $photo->id; ?>">Delete</a>
-                                                <a href="edit_photo.php?id=<?php echo $photo->id; ?>">Edit</a>
-                                                <a href="../photo.php?id=<?php echo $photo->id;?>">View</a>
+                                        <td><?php echo $comment->id; ?></td>
+                                                        
+                                        <td><?php echo $comment->author; ?>
+                                            <div class="action_links">
+                                                <a href="delete_comment.php?id=<?php echo $comment->id; ?>">Delete</a>                                                                                            
                                             </div>
                                         </td>
-                                        <td><?php echo $photo->id; ?></td>
-                                        <td><?php echo $photo->filename; ?></td>
-                                        <td><?php echo $photo->title; ?></td>
-                                        <td><?php echo $photo->size; ?></td>
+                                        <td><?php echo $comment->body; ?></td>
+                                        
                                     </tr>
 
                                     <?php endforeach; ?>
