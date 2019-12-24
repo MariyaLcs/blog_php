796bc66319d6cd5f25936891aa03172fe5ef3a6e
Mariya Lucas <mariya.podosinova@gmail.com>
2019-12-24 20:05:41 +0000

    Setting Up Index to Display Photos

--- index.php
+++ index.php
@@ -1,30 +1,30 @@
 <?php include("includes/header.php"); ?>
+<?php 
+
+$photos = Photo::find_all();
+
+?>
 
 
-        <div class="row">
-
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
-
-    
-            
-          
-         
-
-            </div>
-
-
-
-
-            <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
-
-            
-                 <?php include("includes/sidebar.php"); ?>
-
-
-
+<div class="row">
+    <!-- Blog Entries Column -->
+    <div class="col-md-12">
+        <div class="thumbnails row">
+            <?php foreach($photos as $photo): ?>
+                
+                    <div class="col-xs-6 col-md-3">
+                        <a class="thumbnail" href="">
+                            <img src="admin/<?php echo $photo->picture_path(); ?> " alt="">
+                        </a>
+                    </div>       
+            <?php endforeach;?>
         </div>
-        <!-- /.row -->
+    </div>
+</div>
+    <!-- Blog Sidebar Widgets Column -->
+    <!-- <div class="col-md-4">            
+            <?php //include("includes/sidebar.php"); ?>
+    </div> 
 
-        <?php include("includes/footer.php"); ?>
+
+<?php include("includes/footer.php"); ?>
