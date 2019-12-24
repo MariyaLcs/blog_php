d09b00327a28ecf16700078ff8dfc70eb94dc2d2
Mariya Lucas <mariya.podosinova@gmail.com>
2019-12-24 19:23:33 +0000

    Modifying Chart Properties

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -19,15 +19,18 @@
 
         var data = google.visualization.arrayToDataTable([
           ['Task', 'Hours per Day'],
-          ['Work',     11],
-          ['Eat',      2],
-          ['Commute',  2],
-          ['Watch TV', 2],
-          ['Sleep',    7]
+          ['Views',     <?php echo $session->count(); ?>],
+          ['Photos',      <?php echo $session->count(); ?>],
+          ['Users',  <?php echo $session->count(); ?>],
+          ['Comments', <?php echo $session->count(); ?>],
+          
         ]);
 
         var options = {
-          title: 'My Daily Activities'
+          legend: 'none',
+          pieSliceText: 'label'
+          title: 'My Daily Activities',
+          backgroundColor: 'transparent';
         };
 
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
