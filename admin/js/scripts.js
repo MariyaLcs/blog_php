1631ab55f589018b0f136336a7d63980f151d833
Mariya Lucas <mariya.podosinova@gmail.com>
2019-12-27 17:32:05 +0000

    Pulling User ID with jQuery and Javascript

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
   $(".modal_thumbnails").click(function() {
+    var user_hrerf;
+    var user_href_splitted;
+    var user_id;
+
     $("#set_user_image").prop("disabled", false);
+    user_href = $("#user-id").prop("href");
+    user_href_splitted = user_href.split("=");
+    user_id = user_href_splitted[user_href_splitted.length - 1];
+
+    alert(user_id);
   });
 
   tinymce.init({ selector: "textarea" });
