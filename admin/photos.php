685c5e7b46079e94727a9a7bf10581d4c929731e
Mariya Lucas <mariya.podosinova@gmail.com>
2019-12-29 17:53:49 +0000

    Confirm Delete with Javascript and jQuery

--- admin/photos.php
+++ admin/photos.php
@@ -47,7 +47,7 @@ $photos = Photo::find_all();
                                     <tr>
                                         <td><img class="admin-photo-thumbna" src="<?php echo $photo->picture_path(); ?>" alt="">
                                             <div class="action_link">
-                                                <a href="delete_photo.php?id=<?php echo $photo->id; ?>">Delete</a>
+                                                <a class="delete_link" href="delete_photo.php?id=<?php echo $photo->id; ?>">Delete</a>
                                                 <a href="edit_photo.php?id=<?php echo $photo->id; ?>">Edit</a>
                                                 <a href="../photo.php?id=<?php echo $photo->id;?>">View</a>
                                             </div>
